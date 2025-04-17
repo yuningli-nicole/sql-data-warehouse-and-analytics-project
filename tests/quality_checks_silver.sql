e02aa8346ef09f50a7926db35b6fc75c205d1b23
yuningli-nicole <81702851+yuningli-nicole@users.noreply.github.com>
2025-04-16 18:16:19 -0700

    Update quality_checks_silver.sql

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -23,26 +23,26 @@ Usage Notes:
 -- Expectation: No Result
 
 SELECT cst_id, count(*)
-FROM bronze.crm_cust_info
+FROM silver.crm_cust_info
 GROUP BY cst_id
 HAVING COUNT(*) > 1 OR cst_id IS NULL
 
 -- Check for unwanted spaces
 -- Expectation: No Results
 SELECT cst_firstname
-FROM bronze.crm_cust_info
+FROM silver.crm_cust_info
 WHERE cst_firstname != TRIM(cst_firstname)
 
 SELECT cst_gndr
-FROM bronze.crm_cust_info
+FROM silver.crm_cust_info
 WHERE cst_gndr != TRIM(cst_gndr)
 
 -- Data Standardization & Consistency
 SELECT DISTINCT cst_gndr
-FROM bronze.crm_cust_info
+FROM silver.crm_cust_info
 
 SELECT DISTINCT cst_marital_status
-FROM bronze.crm_cust_info
+FROM silver.crm_cust_info
 
 
 
@@ -52,29 +52,29 @@ FROM bronze.crm_cust_info
 SELECT
 prd_id,
 COUNT(*)
-FROM bronze.crm_prd_info
+FROM silver.crm_prd_info
 GROUP BY prd_id
 HAVING COUNT(*) > 1 OR prd_id IS NULL
 
 -- Check for unwanted spaces
 -- Expectation: No Results
 SELECT prd_nm
-FROM bronze.crm_prd_info
+FROM silver.crm_prd_info
 WHERE prd_nm != TRIM(prd_nm)
 
 -- Check for Nulls or Negative Numbers
 -- Expectation: No Results
 SELECT prd_cost
-FROM bronze.crm_prd_info
+FROM silver.crm_prd_info
 WHERE prd_cost < 0 OR prd_cost IS NULL
 
 -- Data Standardization & Consistency
 SELECT DISTINCT prd_line
-FROM bronze.crm_prd_info
+FROM silver.crm_prd_info
 
 -- Check for Invalid Date Orders
 SELECT * 
-FROM bronze.crm_prd_info
+FROM silver.crm_prd_info
 WHERE prd_end_dt < prd_start_dt
 
 
@@ -84,7 +84,7 @@ WHERE prd_end_dt < prd_start_dt
 -- Check for Invalid Dates
 SELECT
 NULLIF(sls_order_dt, 0) sls_order_dt
-FROM bronze.crm_sales_details
+FROM silver.crm_sales_details
 WHERE sls_order_dt <= 0 
 OR LEN(sls_order_dt) != 8 
 OR sls_order_dt > 20500101 
@@ -93,7 +93,7 @@ OR sls_order_dt < 19000101
 -- Check for Invalid Date Orders
 SELECT
 *
-FROM bronze.crm_sales_details
+FROM silver.crm_sales_details
 WHERE sls_order_dt > sls_ship_dt OR sls_order_dt > sls_due_dt
 
 -- Check for Data Consistency: Between Sales, Quantity, and Price
@@ -111,7 +111,7 @@ CASE WHEN sls_price IS NULL OR sls_price <= 0
         THEN sls_sales / NULLIF(sls_quantity, 0)
      ELSE sls_price
 END AS sls_price
-FROM bronze.crm_sales_details
+FROM silver.crm_sales_details
 WHERE sls_sales != sls_quantity * sls_price
 OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL
 OR sls_sales <= 0 OR sls_quantity <= 0 OR sls_price <= 0
@@ -127,43 +127,31 @@ ORDER BY sls_sales, sls_quantity, sls_price
 -- Identify Out-of-Range Dates
 SELECT DISTINCT 
 bdate
-FROM bronze.erp_cust_az12
+FROM silver.erp_cust_az12
 WHERE bdate < '1924-01-01' OR bdate > GETDATE()
 
 -- Data Standardization & Consistency
--- First, let me update the table to remove the weird trailing spaces after column gen
-UPDATE bronze.erp_cust_az12
-SET gen = REPLACE(REPLACE(REPLACE(gen, CHAR(13), ''), CHAR(10), ''), ' ', '');
-
 SELECT DISTINCT
 gen
-FROM bronze.erp_cust_az12
+FROM silver.erp_cust_az12
 
 
 
 -- erp_loc_a101
--- First, let me update the table to remove the weird trailing spaces after column cntry
-UPDATE bronze.erp_loc_a101
-SET cntry = REPLACE(REPLACE(REPLACE(cntry, CHAR(13), ''), CHAR(10), ''), ' ', '');
-
 -- Data Standardization & Consistency
 SELECT DISTINCT cntry
-FROM bronze.erp_loc_a101
+FROM silver.erp_loc_a101
 ORDER BY cntry
 
 
 
 -- erp_px_cat_g1v2
--- First, let me update the table to remove the weird trailing spaces after column maintenance
-UPDATE bronze.erp_px_cat_g1v2
-SET maintenance = REPLACE(REPLACE(REPLACE(maintenance, CHAR(13), ''), CHAR(10), ''), ' ', '');
-
 -- Check for unwanted spaces
 SELECT *
-FROM bronze.erp_px_cat_g1v2
+FROM silver.erp_px_cat_g1v2
 WHERE cat != TRIM(cat) OR subcat != TRIM(subcat) OR maintenance != TRIM(maintenance)
 
 -- Data Standardization & Consistency
 SELECT DISTINCT
 maintenance
-FROM bronze.erp_px_cat_g1v2
+FROM silver.erp_px_cat_g1v2
