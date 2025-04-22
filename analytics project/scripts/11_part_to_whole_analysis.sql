749038082a606671c25dc8ea1ff9632b9040faf7
yuningli-nicole <81702851+yuningli-nicole@users.noreply.github.com>
2025-04-22 11:34:21 -0700

    Update 11_part_to_whole_analysis.sql

--- analytics project/scripts/11_part_to_whole_analysis.sql
+++ analytics project/scripts/11_part_to_whole_analysis.sql
@@ -26,6 +26,6 @@ SELECT
     category,
     total_sales,
     SUM(total_sales) OVER () AS overall_sales,
-    ROUND((CAST(total_sales AS FLOAT) / SUM(total_sales) OVER ()) * 100, 2) AS percentage_of_total
+    CONCAT(ROUND((CAST (total_sales AS FLOAT) / SUM(total_sales) OVER ()) * 100, 2), '%') AS percentage_of_total
 FROM category_sales
 ORDER BY total_sales DESC;
